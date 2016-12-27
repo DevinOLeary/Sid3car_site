394108d6414354d7d1d62bedd02a9106c83fc221
Devin O'Leary <djoleary07@gmail.com>
2016-12-27 15:23:14 -0500

    removed bootstrap and simple form

--- app/models/inquiry.rb
+++ app/models/inquiry.rb
@@ -1,15 +1,27 @@
+# == Schema Information
+#
+# Table name: inquiries
+#
+#  id         :integer          not null, primary key
+#  name       :string
+#  email      :string
+#  message    :text
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
 class Inquiry < ApplicationRecord
 
+  attr_accessor :name, :email, :content
 
-    attr_accessor :name, :email, :content
+  validates :name,
+    presence: true
 
-    validates :name,
-      presence: true
+  validates :email,
+    presence: true
 
-    validates :email,
-      presence: true
+  validates :content,
+    presence: true
 
-    validates :content,
-      presence: true
 
 end
